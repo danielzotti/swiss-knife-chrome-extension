466b1b0a4e7e7aed4f6295a76dc513dcdadb6c9e
Daniel Zotti <daniel.zotti87@gmail.com>
2020-10-23 18:15:17 +0200

    Add scss and node-sass

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
     <div className="App">
-      This is the app content
+      <textarea placeholder="Insert text here">
+      </textarea>
+      <textarea placeholder="UPPERCASE">
+      </textarea>
+      <textarea placeholder="lowercase">
+      </textarea>
     </div>
   );
 }
