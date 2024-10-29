08cc0d8a3dca72f3eef7e83e5d4aa023e532cb43
Daniel Zotti <daniel.zotti87@gmail.com>
2024-10-29 18:30:06 +0100

    Fix relative import path

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,11 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 
 export default defineConfig({
+  base: './',
   plugins: [react()],
   build: {
     rollupOptions: {
+
       input: {
         popup: 'index.html',
         // background: 'src/background.ts',
@@ -13,7 +15,7 @@ export default defineConfig({
       },
       output: {
         entryFileNames: '[name].js',
-        assetFileNames: '[name].[ext]'
+        assetFileNames: '[name].[ext]',
       }
     }
   }
