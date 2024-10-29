41a7e6b1c43e5d4d7572bea38ecc4523df1191a6
Daniel Zotti <daniel.zotti87@gmail.com>
2024-10-29 17:49:37 +0100

    Fix express

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
 const app = express();
 const port = 5001;
+const __filename = fileURLToPath(import.meta.url); // get the resolved path to the file
+const __dirname = path.dirname(__filename); // get the name of the directory
 // static file serve
 app.use(express.static(__dirname));
 // not found in static files, so default to index.html
